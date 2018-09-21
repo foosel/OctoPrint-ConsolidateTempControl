885f26df76b986a2cdf20058999f50efa79dce5e
jneilliii <jneilliii+github@gmail.com>
2018-09-21 16:16:28 -0400

    0.1.1

--- octoprint_consolidate_temp_control/static/js/consolidate_temp_control.js
+++ octoprint_consolidate_temp_control/static/js/consolidate_temp_control.js
@@ -5,20 +5,36 @@ $(function() {
         self.controlViewModel = parameters[0];
         self.temperatureViewModel = parameters[1];
 		
+		// hide orginal temp and control tab
 		$('#control_link,#temp_link').hide();
-		$('#navbar > div.navbar-inner > div.container').removeClass('container').addClass('row-fluid').css('padding-left','20px');
-		$('#navbar > div.navbar-inner > div.row-fluid > div.nav-collapse').css('padding-right','20px');
+		
+		// page container adjustments
+		$('div.page-container').css({'min-width':'1900px'});
+		
+		// navbar adjustments
+		$('#navbar > div.navbar-inner > div.container').css({'width':'100%'});
+		$('#navbar > div.navbar-inner > div.row-fluid > div.nav-collapse').css({'padding-right':'20px'});
+		
+		// main content adjustments
 		$('div.container.octoprint-container').addClass('row-fluid');
-		$('div.container.octoprint-container.row-fluid > div.row > div.accordion.span4').removeClass('span4').addClass('span2');
-		$('div.container.octoprint-container.row-fluid > div.row').css('margin-left','20px');
+		$('div.container.octoprint-container.row-fluid > div.row').css({'margin-left':'20px','padding-right':'20px'});
+		
+		// sidebar adjustments
+		$('div.container.octoprint-container > div.row > div.accordion.span4').removeClass('span4').addClass('span2');
 		$('#files div.row-fluid.upload-buttons > span.btn.btn-primary.fileinput-button.span6:nth-child(2) > span').text('Upload SD');
+		
+		// tabs adjustments
+		$('div.container.octoprint-container > div.row > div.tabbable.span8').removeClass('span8').addClass('span10');
+		$('div#tabs_content div.tab-pane:not("#tab_plugin_consolidate_temp_control")').wrapInner('<div class="span6"></div>');
+		$('div#tabs_content div.tab-pane:not("#tab_plugin_consolidate_temp_control") div.span6').wrap('<div class="row-fluid"></div>');
+		
+		// footer adjustments
+		$('div.container.octoprint-container > div.footer').css({'padding-left':'20px','padding-right':'20px'});
 
         // fix control tab
         self.onTabChange = function(current, previous) {
-            if (current === "#tab_plugin_consolidate_temp_control") {
+            if ((current === "#tab_plugin_consolidate_temp_control") || (current === "#temp") || (current === "#control")) {
                 var selected = OctoPrint.coreui.selectedTab;
-                OctoPrint.coreui.selectedTab = "#temp";
-                self.controlViewModel.onTabChange("#temp", previous);
                 OctoPrint.coreui.selectedTab = "#control";
                 self.controlViewModel.onTabChange("#control", previous);
                 OctoPrint.coreui.selectedTab = selected;
@@ -33,14 +49,28 @@ $(function() {
                 self.controlViewModel.onAllBound(allViewModels);
                 OctoPrint.coreui.selectedTab = selected;
         };
+		
+        self.controlViewModel.onBrowserTabVisibilityChange = function(status) {
+            if (status) {
+                var selected = OctoPrint.coreui.selectedTab;
+				OctoPrint.coreui.selectedTab = "#control";
+                self.controlViewModel._enableWebcam();
+                OctoPrint.coreui.selectedTab = selected;
+            } else {
+                self.controlViewModel._disableWebcam();
+            }
+        };
 
         // fix temperature tab
         self.onAfterTabChange = function(current, previous) {
-            if (current === "#tab_plugin_consolidate_temp_control") {
+            if ((current === "#tab_plugin_consolidate_temp_control") || (current === "#temp") || (current === "#control")) {
+				if (!self.temperatureViewModel.plot) {
+					self.temperatureViewModel._initializePlot();
+				} else {
+					self.temperatureViewModel.updatePlot();
+				}				
                 self.temperatureViewModel.onAfterTabChange("#temp", previous);
-            } else if (previous === "#tab_plugin_consolidate_temp_control") {
-                self.temperatureViewModel.onAfterTabChange(current, "#temp");
-            }
+            } 
         }
     }
 
